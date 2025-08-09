3edcec52874c152d3ddd9c9ac46a3b9f019252f0
Lin <tzu.hsun.lin046@gmail.com>
2025-08-10 03:52:52 +0800

    update link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-week1/',
+  base: '/vue-week1-hw/',
   plugins: [
     vue(),
     vueDevTools(),
